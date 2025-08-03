66f54e62cad0fcbf1f725c6e375b09c14b0d93df
Juan <juanmelgar01@gmail.com>
2025-08-03 18:39:48 +0200

    Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,37 @@
 import styles from '@/styles/navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
     return (
         <div className={styles.contenedorCabecera}>
             <div>
                 <Image src="/images/LogoLuzem.png"
-                        alt="Logo Luzem"
-                        width={150}
-                        height={56}
+                    alt="Logo Luzem"
+                    width={150}
+                    height={56}
                 />
             </div>
-            <nav className={styles.menu}>
-            <ul>
-                <li><Link href='/'>Inicio</Link></li>
-                <li><Link href='/Sesiones'>Galeria</Link></li>
-                <li><Link href='/Portfolio'>Portfolio</Link></li>
-                <li><Link href='/SobreMi'>Sobre mí</Link></li>
-                <li><Link href='/Contacto'>Contacto</Link></li>
-            </ul>
-        </nav>
+            <div className={styles.menuMv} onClick={toggleMenu}>
+                <div/><div/><div/>
+            </div>
+            <nav className={`${styles.menu} ${menuOpen ? styles.open : ''}`}>
+                <ul>
+                    <li><Link href='/'>Inicio</Link></li>
+                    <li><Link href='/Sesiones'>Sesiones</Link></li>
+                    <li><Link href='/Portfolio'>Portfolio</Link></li>
+                    <li><Link href='/SobreMi'>Sobre mí</Link></li>
+                    <li><Link href='/Contacto'>Contacto</Link></li>
+                </ul>
+            </nav>
 
         </div>
-        
+
     )
 }
